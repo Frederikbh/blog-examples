a9089d7d76993adfee0d501eff5c12275d6feebf
Frederik Baun Hansen <frederikbaun@hotmail.dk>
2025-05-11 10:55:48 +0200

    Added Kiota Petstore sample

---
+++ samples/api-client-kiota/ApiClientKiota.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Clients.Petstore;
+using Clients.Petstore.Models;
+
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Bundle;
+
+var requestAdapter = new DefaultRequestAdapter(new AnonymousAuthenticationProvider());
+var client = new PetstoreClient(requestAdapter);
+
+var pet = new Pet
+{
+    Id = 123,
+    Name = "Sir Barksalot",
+    Status = Pet_status.Available
+};
+
+await client.Pet.PostAsync(pet);
+
+var createdPet = await client.Pet[pet.Id!.Value].GetAsync();
+
+Console.ReadLine();
